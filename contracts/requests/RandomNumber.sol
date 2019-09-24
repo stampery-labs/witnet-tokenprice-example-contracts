aa230b64a019baf7c9116ba9f1f7607f54cabbe3
Mario Cao <mariocao@gmail.com>
2019-09-24 13:06:48 +0200

    chore: unbox witnet truffle box

---
+++ contracts/requests/RandomNumber.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.0;
+
+import "witnet-ethereum-bridge/contracts/Request.sol";
+
+// The bytecode of the RandomNumber request that will be sent to Witnet
+contract RandomNumberRequest is Request {
+  constructor () Request(hex"0a710880dd9f9c0612511238687474703a2f2f71726e672e616e752e6564752e61752f4150492f6a736f6e492e7068703f6c656e6774683d3126747970653d75696e74381a158618451874821861646461746118708218550018731a030a018022110a0f851854185018751873821827190100100018022001280030003800") public { }
+}
