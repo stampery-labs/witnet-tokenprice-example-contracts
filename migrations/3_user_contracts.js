a06cb49bd6c7cc9bc0930eb1e99df78be34b2318
Mario Cao <mariocao@gmail.com>
2019-09-24 15:46:26 +0200

    chore: clean up truffle box with new version

--- migrations/3_user_contracts.js
+++ migrations/3_user_contracts.js
@@ -2,13 +2,9 @@
 // each contracts' constructor arguments (you can freely edit those and the compiler will respect them).
 const Witnet = artifacts.require("Witnet")
 const WitnetBridgeInterface = artifacts.require("WitnetBridgeInterface")
-const PriceFeed = artifacts.require("PriceFeed")
-const QuantumDice = artifacts.require("QuantumDice")
-const WeatherContest = artifacts.require("WeatherContest")
+
 
 module.exports = function (deployer) {
-  deployer.link(Witnet, [PriceFeed, QuantumDice, WeatherContest])
-  deployer.deploy(PriceFeed, WitnetBridgeInterface.address, 0)
-  deployer.deploy(QuantumDice, WitnetBridgeInterface.address, 0)
-  deployer.deploy(WeatherContest, WitnetBridgeInterface.address)
+  deployer.link(Witnet, [])
+
 }
